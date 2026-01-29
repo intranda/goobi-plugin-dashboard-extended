598e5e8f2f7f8d41c3468c112fc0c0de2ac29878
Simon Sendler <simon.sendler@intranda.com>
2026-01-29 09:23:41 +0100

    build: update build system for current version of svelte
    re #27533

--- module-gui/src/main/webapp/svelte-tags/src/main.js
+++ module-gui/src/main/webapp/svelte-tags/src/main.js
@@ -1,6 +1,7 @@
+import { mount } from 'svelte';
 import Nagios from './Nagios.svelte';
 import Icinga2 from './Icinga2.svelte';
 
 
-window.mountSvelteNagios = Nagios;
-window.mountSvelteIcinga2 = Icinga2;
+window.mountSvelteNagios = (options) => mount(Nagios, options);
+window.mountSvelteIcinga2 = (options) => mount(Icinga2, options);
